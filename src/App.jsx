a8afdf0650cd59e153adfbfb499ba7c57b33c5d4
WindowsM16a <wuvershayne@gmail.com>
2024-12-15 19:35:58 +0000

    pause, got tailwind working

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import "./index.css";
-
 function App() {
   return (
     <>
-      <div className="container flex flex-col w-4 border-sky-300">
-        <div className="product_card">
-          <div className="product_img">
+      <div className="container border-sky-300">
+        <div className="product_card w-37 flex-row  bg-white rounded-3xl">
+          <div className="product_img w-50% h-100%">
             <img
-              className="product_img"
-              src="src/assets/image-produt-mobile.jpg"
+              className="product_img w-100% h-100%"
+              src="src/assets/image-product-mobile.jpg"
               alt="perfume-pic"
             />
           </div>
@@ -19,10 +17,13 @@ function App() {
               A floral, solar and voluptuous interpretation composed by Olivier
               Polge, Perfumer-Creator for the House of CHANEL.
             </p>
-            <div className="prices flex flex-col ">
-              <p>$149.99</p>
-              <s>$169.99</s>
+            <div className="prices flex flex-row items-center justify-center gap-3">
+              <p className="text-3xl">$149.99</p>
+              <s className="line-through">$169.99</s>
             </div>
+            <button className="btn w-8">
+              <img src="src/assets/icon-cart.svg" alt="cart svg" /> Add to Cart
+            </button>
           </div>
         </div>
       </div>
